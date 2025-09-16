368cea58fe084b5aa9dcebf32634f1d73d953b1e
pakutonzz <patkultornchairat@gmail.com>
2025-09-16 21:50:13 +0700

    cell01

---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Patkultorn"
+last_name = "Chairat"
+whole_name = first_name + " " + last_name
+print(whole_name)
---
+++ cell01/ex02/age.py
@@ -0,0 +1,4 @@
+age = 20
+seesong = 42
+my_age = age + seesong
+print(my_age)
