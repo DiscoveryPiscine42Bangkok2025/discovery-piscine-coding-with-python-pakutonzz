a93a971be61bb1ff7e89ff99e23c6a51fc77cf64
pakutonzz <patkultornchairat@gmail.com>
2025-09-18 01:46:57 +0700

    mod to submit

--- cell03/ex01/multiplication_table.py
+++ cell03/ex01/multiplication_table.py
@@ -1,10 +1,5 @@
-#!/usr/bin/env python3
 
-def main():
-    num = int(input("Enter a number\n"))
+num = int(input("Enter a number\n"))
 
-    for i in range(0, 10):
-        print(i, "*", num, "=", i * num)
-
-if __name__ == "__main__":
-    main()
+for i in range(0, 10):
+    print(i, "*", num, "=", i * num)
